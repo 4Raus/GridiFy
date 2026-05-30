ce127995c8ddd1363efebde9f412ec0377a511e4
4Raus <151329032+4Raus@users.noreply.github.com>
2026-05-30 17:00:30 +0500

    Update externalLinks.ts

--- ui-library-front/src/shared/config/externalLinks.ts
+++ ui-library-front/src/shared/config/externalLinks.ts
@@ -1,4 +1,4 @@
 export const externalLinks = {
-  figma: 'https://www.figma.com/',
-  github: 'https://github.com/',
+  figma: 'https://www.figma.com/design/4cHJUxz5OpwfRlk2AJGepN/Diplome?node-id=0-1&t=moYNoTVzzWx0LvD8-1',
+  github: 'https://github.com/4Raus/GridiFy',
 } as const;
